ad5e1f7ce49fea2b6188d8447d88a87c629dfe03
David Saavedra <davideloymartinez@gmail.com>
2022-10-02 19:21:58 -0700

    added new AddNewStudent component and route

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import AddNewStudent from './components/AddNewStudent';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <AppBar position="static" color="default">
         <Toolbar>
-           <Typography variant="h6" color="inherit">
+           <Typography variant="h6" color="inherit" style={{flexGrow: 1, textAlign: "left"}}>
             Course Registration
            </Typography>
+            <Button component={Link} to="/">Home</Button> 
+            <Button component={Link} to="/addstudent">Add Student</Button>
         </Toolbar>
       </AppBar>
-      <BrowserRouter>
        <Switch>
         <Route exact path='/' component={Semester} />
         <Route path='/schedule' component={SchedList} />
+        <Route path='/addstudent' component={AddNewStudent} />
        </Switch>
       </BrowserRouter>
     </div>
